7eb1fb377d21c42e11914007ff6d3a03bee4bb54
DASNET México <88397393+dasnet99@users.noreply.github.com>
2021-09-23 22:25:47 -0500

    Update RepositorioMaestro.cs

--- RepositorioMaestro.cs
+++ RepositorioMaestro.cs
@@ -11,7 +11,7 @@ namespace SoftwareDasnet.CapaDatos
     public abstract class RepositorioMaestro:Repositorio
     {
         protected List<SqlParameter> parametros;
-        protected void ExecuteNonQuery(string transactSql)
+        protected void ExecuteNonQuery(string transactSql) //Ejecutar sentencias de texto insert, update, delete con parametros
         {
             using (var conexion = ObtenerConexion())
             {
@@ -30,7 +30,60 @@ namespace SoftwareDasnet.CapaDatos
                 }
             }
         }
-        protected DataTable ExecuteReader(string transactSql)
+        
+        protected void ExecuteNonQuery(string transactSql) //Ejecutar sentencias de texto insert, update, delete etc sin parametros
+        {
+            using (var conexion = ObtenerConexion())
+            {
+                conexion.Open();
+                using (var comando = new SqlCommand())
+                {
+                    comando.Connection = conexion;
+                    comando.CommandText = transactSql;
+                    comando.CommandType = CommandType.Text;
+                    comando.ExecuteNonQuery();
+                    parametros.Clear();
+                }
+            }
+        }
+        
+        protected void ExecuteNonQuery(string transactSql) //Ejecutar procedimientos almacenados sin parametros
+        {
+            using (var conexion = ObtenerConexion())
+            {
+                conexion.Open();
+                using (var comando = new SqlCommand())
+                {
+                    comando.Connection = conexion;
+                    comando.CommandText = transactSql;
+                    comando.CommandType = CommandType.StoredProcedure;
+                    comando.ExecuteNonQuery();
+                    parametros.Clear();
+                }
+            }
+        }
+        
+        protected void ExecuteNonQuery(string transactSql) //Ejecutar procedimientos almacenados con parametros
+        {
+            using (var conexion = ObtenerConexion())
+            {
+                conexion.Open();
+                using (var comando = new SqlCommand())
+                {
+                    comando.Connection = conexion;
+                    comando.CommandText = transactSql;
+                    comando.CommandType = CommandType.StoredProcedure;
+                    foreach (SqlParameter item in parametros)
+                    {
+                        comando.Parameters.Add(item);
+                    }
+                    comando.ExecuteNonQuery();
+                    parametros.Clear();
+                }
+            }
+        }
+        
+        protected DataTable ExecuteReader(string transactSql) //Devolver tablas ejecutando consultas de texto con parametros
         {
             using (var conexion = ObtenerConexion())
             {
@@ -44,6 +97,73 @@ namespace SoftwareDasnet.CapaDatos
                     {
                         comando.Parameters.Add(item);
                     }
+                    SqlDataReader lector = comando.ExecuteReader();
+                    using (var tabla = new DataTable())
+                    {
+                        tabla.Load(lector);
+                        lector.Dispose();
+                        return tabla;
+                    }
+                }
+            }
+        }
+        
+        protected DataTable ExecuteReader(string transactSql) //Devolver tablas ejecutando consultas de texto sin parametros
+        {
+            using (var conexion = ObtenerConexion())
+            {
+                conexion.Open();
+                using (var comando = new SqlCommand())
+                {
+                    comando.Connection = conexion;
+                    comando.CommandText = transactSql;
+                    comando.CommandType = CommandType.Text;
+                    SqlDataReader lector = comando.ExecuteReader();
+                    using (var tabla = new DataTable())
+                    {
+                        tabla.Load(lector);
+                        lector.Dispose();
+                        return tabla;
+                    }
+                }
+            }
+        }
+        
+        protected DataTable ExecuteReader(string transactSql) //Devolver tablas ejecutando consultas de procedimientos almacenados con parametros
+        {
+            using (var conexion = ObtenerConexion())
+            {
+                conexion.Open();
+                using (var comando = new SqlCommand())
+                {
+                    comando.Connection = conexion;
+                    comando.CommandText = transactSql;
+                    comando.CommandType = CommandType.StoredProcedures;
+                    foreach (SqlParameter item in parametros)
+                    {
+                        comando.Parameters.Add(item);
+                    }
+                    SqlDataReader lector = comando.ExecuteReader();
+                    using (var tabla = new DataTable())
+                    {
+                        tabla.Load(lector);
+                        lector.Dispose();
+                        return tabla;
+                    }
+                }
+            }
+        }
+        
+        protected DataTable ExecuteReader(string transactSql) //Devolver tablas ejecutando procedimientos almacenados sin parametros
+        {
+            using (var conexion = ObtenerConexion())
+            {
+                conexion.Open();
+                using (var comando = new SqlCommand())
+                {
+                    comando.Connection = conexion;
+                    comando.CommandText = transactSql;
+                    comando.CommandType = CommandType.StoredProcedure;
                     SqlDataReader lector = comando.ExecuteReader();
                     using (var tabla = new DataTable())
                     {
