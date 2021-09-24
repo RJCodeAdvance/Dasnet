618069e65a36d3fd0d161da498c0ea0d8f04a505
DASNET México <88397393+dasnet99@users.noreply.github.com>
2021-09-23 21:50:05 -0500

    Add files via upload

---
+++ Repositorio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareDasnet.CapaDatos
+{
+    public abstract class Repositorio
+    {
+        private readonly string CadenaConexion;
+        public Repositorio()
+        {
+            CadenaConexion = "server = DESKTOP-S6SO1ER\\SQLEXPRESS; database=WppDasnet; integrated security=true";
+        }
+        protected SqlConnection ObtenerConexion()
+        {
+            return new SqlConnection(CadenaConexion);
+        }
+    }
+}
---
+++ RepositorioMaestro.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareDasnet.CapaDatos
+{
+    public abstract class RepositorioMaestro:Repositorio
+    {
+        protected List<SqlParameter> parametros;
+        protected void ExecuteNonQuery(string transactSql)
+        {
+            using (var conexion = ObtenerConexion())
+            {
+                conexion.Open();
+                using (var comando = new SqlCommand())
+                {
+                    comando.Connection = conexion;
+                    comando.CommandText = transactSql;
+                    comando.CommandType = CommandType.Text;
+                    foreach (SqlParameter item in parametros)
+                    {
+                        comando.Parameters.Add(item);
+                    }
+                    comando.ExecuteNonQuery();
+                    parametros.Clear();
+                }
+            }
+        }
+        protected DataTable ExecuteReader(string transactSql)
+        {
+            using (var conexion = ObtenerConexion())
+            {
+                conexion.Open();
+                using (var comando = new SqlCommand())
+                {
+                    comando.Connection = conexion;
+                    comando.CommandText = transactSql;
+                    comando.CommandType = CommandType.Text;
+                    foreach (SqlParameter item in parametros)
+                    {
+                        comando.Parameters.Add(item);
+                    }
+                    SqlDataReader lector = comando.ExecuteReader();
+                    using (var tabla = new DataTable())
+                    {
+                        tabla.Load(lector);
+                        lector.Dispose();
+                        return tabla;
+                    }
+                }
+            }
+        }
+    }
+}
